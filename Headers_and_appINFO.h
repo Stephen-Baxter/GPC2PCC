b8703123819001d5625612f02335b5c758e42ae7
Stephen-Baxter <52463998+Stephen-Baxter@users.noreply.github.com>
2019-09-10 11:00:43 -0500

    Create Headers_and_appINFO.h

---
+++ Headers_and_appINFO.h
@@ -0,0 +1,24 @@
+#pragma once
+#pragma comment(lib, "XInput.lib")
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <Windows.h>
+#include <Xinput.h>
+#include <conio.h> //For _getch();
+
+#include "resource.h"
+
+#define PI 3.1415926535897932384626433832795
+
+//******************************************************************************************************
+//Author: Stephen Baxter
+//Development Tools Used: Visual Studeo 2019
+//Name: GPC2PCC
+//Language: c++
+//
+//Description: Game Pad Controls To Personal Computer Controls (GPC2PCC) is an application that allows
+//game pads to be used as a keyboard and mouse on a computer
+//******************************************************************************************************
